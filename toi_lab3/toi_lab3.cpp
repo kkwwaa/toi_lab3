e81ee695f48d914bb3a1b5de29d61092bd3d294c
kkwwaa <katyabasova20062@gmail.com>
2025-03-12 03:22:57 +0500

    Tree and RPS done

--- toi_lab3/toi_lab3.cpp
+++ toi_lab3/toi_lab3.cpp
@@ -4,21 +4,47 @@
 using namespace std;//!!! Скобки у отриц не обязательны для удобства и униерсальности. Отрицательные выр-ия не пред-ны, домножайте на -1
 
 int sn = 0;
-string s = "(9-2)=(q/2)";
+string s = "(4)=(9)";
+//9-2)=(q/2)";
 bool bad = false;
-bool in_comp = false;
+bool in_comp = false, sign = false;
 
-void PartOfComparativeExpression();
-void Expression();
-void Addend();
-void Factor();
-void Number();
-void SignedNumber();
+#pragma region Additional
+struct TreeNode {
+    string inf = "";
 
-#pragma region MyRegion
+    TreeNode* left = nullptr;
+    TreeNode* right = nullptr;
+};
+TreeNode* Root = nullptr;
+TreeNode* RootComp = nullptr;
+
+void Insert(TreeNode*& R) {
+    R = new TreeNode;
+    R->inf = s[sn];
+}
+
+void Delete(TreeNode*& R) {
+    if (R != nullptr) {
+        Delete(R->left);
+        Delete(R->right);
+        delete R;
+    }
+}
+
+void Write(TreeNode*& R) {
+    if (R != nullptr) {
+        Write(R->left);
+        Write(R->right);
+        cout << R->inf << ' ';
+    }
+}
+
 void Error(string s) {
     if (!bad) cout << "Обнаружена ошибка в выражении: " << s << "\nПрограмма прекращает работу\n";
-}
+    Delete(Root);
+    bad = true;
+} //добавить ошибку с пробелами
 
 // Обработка перехода
 void next() {
@@ -40,238 +66,252 @@ bool is_digit(char c) {
 bool is_alpha(char c) {
     return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
 }
+#pragma endregion
 
+#pragma region Voids
+void PartOfComparativeExpression(TreeNode*& R);
+void Expression(TreeNode*& R);
+void Addend(TreeNode*& R);
+void Factor(TreeNode*& R);
+void Number(TreeNode*& R);
+void SignedNumber(TreeNode*& R);
+#pragma endregion
+
+#pragma region Numbers
 // <Number> ::= <Digit> {<Digit> | '0'}
-void Number() {
-    if (!is_digit1_9(s[sn])) {
-        Error("Число должно начинаться с 1-9");
-        bad = true;
-        return;
-    }
+void Number(TreeNode*& R) {
+    Insert(R);
+    if (sign) R->inf = '-' + R->inf;
     next();
 
     while (sn != -1 && is_digit(s[sn])) {
+        R->inf += s[sn];
         next();
     }
 }
 
 // <Double_Number> ::= (<Digit> | '0') '.' {(<Digit> | '0')}
-void PartOfDoubleNumber() {
+void PartOfDoubleNumber(TreeNode*& R) {
+    Insert(R);
     next();
 
     if (sn == -1 || !is_digit(s[sn])) {
         Error("Ожидалась цифра после '.' в дробном числе");
-        bad = true;
         return;
     }
 
     while (sn != -1 && is_digit(s[sn])) {
+        R->inf += s[sn];
         next();
     }
 }
 
 // <Sign_Number> ::= '-' (<Number> | <Double_Number>)
-void SignedNumber() {
+void SignedNumber(TreeNode*& R) {
+    sign = true;
     next();
 
     if (sn == -1) {
         Error("Нет числа или переменной после '-'");
-        bad = true;
         return;
     }
 
     if (is_digit(s[sn])) {
         if (s[sn] == '0') {
+            R->inf += s[sn];
             next();
-            if (sn != -1 && s[sn] == '.') PartOfDoubleNumber();
+            if (sn != -1 && s[sn] == '.') PartOfDoubleNumber(R);
             else {
                 Error("Ожидалась '.' в знаковом дробном числе");
-                bad = true;
                 return;
             }
         }
         else {
-            Number();
-            if (sn != -1 && s[sn] == '.') PartOfDoubleNumber();
+            Number(R);
+            if (sn != -1 && s[sn] == '.') PartOfDoubleNumber(R);
         }
     }
-    else if (is_alpha(s[sn])) next();
+    else if (is_alpha(s[sn])) {
+        Insert(R);
+        next();
+    }
     else {
         Error("Неверное знаковое число");
-        bad = true;
     }
+    sign = false;
 }
+#pragma endregion
 
 // <Factor> ::= <Letter> | <Number> | '(' <Expression> ')' | <Sign_Number>
-void Factor() {
+void Factor(TreeNode*& R) {
     if (sn == -1) {
         Error("Конец строки в множителе");
-        bad = true;
         return;
     }
 
     if (s[sn] == '(') {
         if (sn == 0) in_comp = true;
         next();
-        Expression();
+        Expression(R);
         if (sn == -1 || s[sn] != ')') {
             Error("Отсутствует закрывающая скобка в множителе");
-            bad = true;
             return;
         }
         next();
     }
     else if (s[sn] == '-') {
-        SignedNumber();
+        SignedNumber(R);
     }
     else if (is_digit(s[sn])) {
         if (s[sn] == '0') {
+            Insert(R);
             next();
-            if (sn != -1 && s[sn] == '.') PartOfDoubleNumber();
+            if (sn != -1 && s[sn] == '.') PartOfDoubleNumber(R);
             else if (is_digit(s[sn])){
                 Error("Отсутствует точка после 0 в числе");
-                bad = true;
                 return;
             }
         }
         else {
-            Number();
-            if (sn != -1 && s[sn] == '.') PartOfDoubleNumber();
+            Number(R);
+            if (sn != -1 && s[sn] == '.') PartOfDoubleNumber(R);
         }
     }
     else if (is_alpha(s[sn])) {
+        Insert(R);
         next();
     }
     else {
         Error("Неверный множитель");
-        bad = true;
     }
 }
 
 // <Addend> ::= <Factor> {('*' | '/') <Factor>}
-void Addend() {
-    Factor();
+void Addend(TreeNode*& R) {
+    Factor(R);
     if (bad || sn == -1) return;
 
     while (sn != -1 && (s[sn] == '*' || s[sn] == '/')) {
+        TreeNode* O = new TreeNode;
+        Insert(O);
+        O->left = R;
         next();
-        Factor();
+        Factor(O->right);
+        R = O;
     }
 }
 
 // <Expression> ::= <Addend> {('+' | '-') <Addend>}
-void Expression() {
-    Addend();
-    if (bad || sn == -1) return;
+void Expression(TreeNode*& R) {
+    Addend(R);
+    TreeNode* O = R;
 
     while (sn != -1 && (s[sn] == '+' || s[sn] == '-')) {
+        TreeNode* O = new TreeNode;
+        Insert(O);
+        O->left = R;
         next();
-        Addend();
+        Addend(O->right);
+        R = O;
     }
 }
 
+#pragma region Expressions
 // <PartOfComparativeExpression> ::= ('>' | '<' | '=') '('Expression')'
-void PartOfComparativeExpression() {
+void PartOfComparativeExpression(TreeNode*& R){
     if (sn == -1 || !(s[sn] == '>' || s[sn] == '<' || s[sn] == '=')) {
         Error("Неверный оператор сравнения");
-        bad = true;
         return;
     }
+    TreeNode* O = new TreeNode;
+    Insert(O);
+    O->left = R;
     next();
-
+    
     if (sn == -1 || s[sn] != '(') {
         Error("Ожидалась '(' после оператора сравнения");
-        bad = true;
         return;
     }
     next();
-    Expression();
+    Expression(O->right);
     if (sn == -1 || s[sn] != ')') {
         Error("Отсутствует закрывающая скобка после первого выражения");
-        bad = true;
         return;
     }
+    R = O;
     next();
 }
 
-void BooleanExpressionNot() {
+void BooleanExpressionNot(TreeNode*& R) {
     if (s[sn] == 'н') {
         next();
         if (sn != -1 && s[sn] == 'е') {
             next();
             if (sn != -1 && s[sn] == '(') {
                 next();
-                Expression();
-                PartOfComparativeExpression();
+                Expression(R);
+                PartOfComparativeExpression(R);
                 if (sn != -1 && s[sn] == ')') next();
                 else {
                     Error("Отсутствует закрывающая скобка после 'не'");
-                    bad = true;
                 }
             }
             else {
                 Error("Ожидалась '(' после 'не'");
-                bad = true;
             }
         }
         else {
             Error("Ожидалась 'е' после 'н' для 'не'");
-            bad = true;
         }
     }
 }
 
-void BooleanExpression() {
+void BooleanExpression(TreeNode*& R) {
     if (sn != -1 && s[sn] == '(') {
         next();
-        Expression();
+        Expression(R);
         if (sn != -1 && s[sn] == ')') {
             next();
-            PartOfComparativeExpression();
+            PartOfComparativeExpression(R);
             if (sn != -1 && s[sn] == ',') {
                 next();
                 if (sn != -1 && s[sn] == '(') {
                     next();
-                    Expression();
+                    Expression(R);
                     if (sn != -1 && s[sn] == ')') {
                         next();
-                        PartOfComparativeExpression();
+                        PartOfComparativeExpression(R);
                         if (sn != -1 && s[sn] == ')') next();
                         else {
                             Error("Отсутствует закрывающая скобка после второго выражения в 'и'");
-                            bad = true;
                         }
                     }
                     else {
                         Error("Ожидалась ')' в выражении 'и'");
-                        bad = true;
                     }
                 }
                 else {
                     Error("Ожидалась ',' в выражении 'и'");
-                    bad = true;
                 }
             }
             else {
                 Error("Отсутствует закрывающая скобка после первого выражения в 'и'");
-                bad = true;
             }
         }
         else {
             Error("Отсутствует открывающая скобка первого выражения в 'и'");
-            bad = true;
         }
     }
 }
-#pragma endregion
 
-void BooleanExpressionAndOr() {
+
+void BooleanExpressionAndOr(TreeNode*& R) {
     // Проверка "и" & "или"
     if (sn != -1 && s[sn] == 'и') {
         next();
         if (sn != -1 && s[sn] == '(') {
             next();
-            BooleanExpression();
+            BooleanExpression(R);
         }
         else if (sn != -1 && s[sn] == 'л') {
             next();
@@ -279,7 +319,7 @@ void BooleanExpressionAndOr() {
                 next();
                 if (sn != -1 && s[sn] == '(') {
                     next();
-                    BooleanExpression();
+                    BooleanExpression(R);
                 }
                 else {
                     Error("Ожидалась '(' после 'или'");
@@ -297,6 +337,9 @@ void BooleanExpressionAndOr() {
         }
     }
 }
+#pragma endregion
+
+
 
 int main() {
     setlocale(LC_ALL, "Russian");
@@ -305,13 +348,13 @@ int main() {
 
     cout << s << '\n';
 
-    BooleanExpressionNot();
-    BooleanExpressionAndOr();
+    BooleanExpressionNot(Root);
+    BooleanExpressionAndOr(Root);
 
     // Первичный вызов
-    if (sn!=-1)Expression();
+    if (sn!=-1)Expression(Root);
     //Если найдено сравнение, то переквалифицируем в сравнительное выражение
-    if (in_comp && sn != -1) PartOfComparativeExpression();
+    if (in_comp && sn != -1) PartOfComparativeExpression(Root);
 
     // Проверка на оставшиеся символы
     if (sn != -1) {
@@ -320,6 +363,7 @@ int main() {
     }
 
     if (!bad) cout << "Строка корректна\n";
+    if (Root) Write(Root);
 
     return 0;
     
